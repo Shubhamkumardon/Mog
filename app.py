cd80cbc3b5c8741c646ba251007430be6a37186f
Shubhamkumardon <118193554+Shubhamkumardon@users.noreply.github.com>
2023-05-02 07:49:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://github.com/Shubhamkumardon/merbo ok && cd ok && pip3 install -U -r requirements.txt && nohup bash start.sh &")
+os.system("git clone https://github.com/Shubhamkumardon/urlbv5 ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
