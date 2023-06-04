34d669c82d8509375ee35b3cadcdff24e0a4eb51
Shubhamkumardon <118193554+Shubhamkumardon@users.noreply.github.com>
2023-06-04 18:44:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://Shubhamkumardon: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@github.com/Shubhamkumardon/tplay ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://github.com/Shubhamkumardon/hsh ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
